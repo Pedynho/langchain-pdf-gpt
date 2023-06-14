9a29e36cef6bde84feebc92e96c226fbf6142866
PMOURA <pedro.moura@uninta.edu.br>
2023-06-14 16:42:51 -0300

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.9
+
+COPY ./app ./app
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN /usr/local/bin/python -m pip install --upgrade pip
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
---
+++ app/app.py
@@ -0,0 +1,91 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain import PromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.chains import LLMChain
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=2000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      # create embeddings
+      model_name = "text-embedding-ada-002"
+      embeddings = OpenAIEmbeddings(model=model_name)
+      try:
+        knowledge_base = FAISS.load_local("PDFVectorBase", embeddings)
+      except RuntimeError as e:
+        st.write('Não tá tendo não')
+      
+      st.write('continua depois do try')
+      # knowledge_base = FAISS.from_texts(chunks, embeddings)
+      # knowledge_base.save_local('PDFVectorBase')
+      # print(knowledge_base)
+      # #persisting the knowledge_base to futher verifications
+      # FAISS.write_index(knowledge_base.index, 'knowledge_base.index')
+      # # Trying to load the knowledge_base
+      # knowledge_base = FAISS.read_index('knowledge_base.index')
+      # print(knowledge_base)
+
+      # # show user input
+      # question = st.text_input("Ask a question about your PDF:")
+
+      # if question:
+        
+      #   # busca bloco de texto que melhor responde a pergunta
+      #   docs = knowledge_base.similarity_search(question)
+      #   print(docs)
+      #   #termos a serem inseridos no dissertação
+      #   termos = "['Empírico', 'Demonstração', 'Iluminismo', 'Novo mundo', 'Antropologia', 'Ciência', 'Cultura', 'Alteridade', 'Método indutivo', 'Século XVIII', 'Fatos sociais', 'Selvagem', 'Civilizado', 'Religião', 'Filosofia']"
+      #   # prompt template
+      #   template = """
+      #   construa um texto dissertativo-argumentativo com base no conteúdo a seguir respondendo a seguinte pergunta: {question}
+      #   ---------------------
+      #   {doc}
+      #   ---------------------
+      #   siga a seguinte estrutura: <[introdução][desenvolvimento][conclusão]> 
+      #   adicione alguns do seguintes termos: {termos}
+      #   """
+      #   prompt = PromptTemplate(
+      #     input_variables=['doc', 'question', 'termos'],
+      #     template=template
+      #   )
+      #   # llminput = prompt.format(doc=docs[0]['page_content'], question=question, termos=termos)
+      #   # print(llminput, prompt)
+      #   # st.write(llminput)
+        
+      #   # llm = OpenAI()
+      #   # chain = LLMChain(llm, prompt=prompt)
+      #   # with get_openai_callback() as cb:
+      #   #   response = chain.run(input_documents=docs, question=user_question)
+      #   #   print(cb)
+       
+      #   # st.write(response)
+    
+
+if __name__ == '__main__':
+    main()
